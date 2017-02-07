89bc38f435c59b12a536e56e56786e820aad664c
Dom Fradley <dom@techiezealot.com>
2017-02-07 14:51:15 +0000

    Delete resource_group_creation.ps1

--- resource_group_creation.ps1
+++
@@ -1,8 +0,0 @@
-##Azure Resource Group Creation Script
-##
-## Set the Resource Group name
-$RGName = Read-Host -Prompt "Resource Group Name"
-##Set the Resource Group location
-$RGLocation = Read-Host -Prompt "Resource Group Location"
-
-New-AzureRMResourceGroup -Name $RGName -Location $RGLocation -Tag $RGTag
